4acb19949bbd96a5febbf692299a9268a43513b4
Shriyash05 <shrimhatre00@gmail.com>
2024-01-13 14:14:48 +0530

    config

--- azconfig.sh
+++ azconfig.sh
@@ -6,4 +6,4 @@ PLANSKU="F1"
 SITENAME="duotrivia"
 RUNTIME="PYTHON|3.12"
 
-az webapp config set --resource-group $RESOURCEGROUP --name $SITENAME --startup-file "unicorn app.main:app --host 0.0.0.0 --port 8080
+az webapp config set --resource-group $RESOURCEGROUP --name $SITENAME --startup-file "unicorn app.main:app --host 0.0.0.0 --port 8080"
